d2419e96576a2378471043f475045b2b47753b65
EduardoFariaKruger <eduardofkruger@gmail.com>
2021-11-25 23:17:35 -0300

    links funcionando

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from datetime import datetime
+import time
 import webbrowser
 
-url = 'https://github.com/EduardoFariaKruger/classTimer'
-now = datetime.now().time()
+current_time = str()
+deactivator = False
+time1 = datetime(year=2021, month=11, day=25, hour=23, minute=17, second=0)
+now = datetime
 
-print('now =', now)
-print('type(now) =', type(now))
-webbrowser.open(url)
-print(url)
+hr1 = datetime.strftime(time1, "%H,%M,%S")
+
+def SetTime():
+    time.sleep(1)
+    timeNow = datetime.now()
+    now = datetime.strftime(timeNow, "%H,%M,%S")
+    return now
+
+while deactivator == False:
+    now = SetTime()
+    print(now)
+    if now == hr1:
+        webbrowser.open("https://github.com/EduardoFariaKruger/classTimer")
+        deactivator = True
+        break
+    
---
+++ teste.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+import time
+import webbrowser
+
+webbrowser.open("https://github.com/EduardoFariaKruger/classTimer")
