df82ac7549982d91ac3271e4cf45f5c5a5a7a6d2
EduardoFariaKruger <eduardofkruger@gmail.com>
2021-11-30 11:11:13 -0300

    codigo reduzido

--- scripts/horarios.py
+++ scripts/horarios.py
@@ -8,7 +8,6 @@ import requests
 
 a = True
 lista = []
-dia = str()
 current = date.today()
 currentDay = datetime.strftime(current, "%A")
 
@@ -38,128 +37,41 @@ quimica = ''
 historia = ''
 edfisica = ''
 espanhol = ''
+projeto = ''
 
-
-diviser = int(2)
-quantity = int()
 def getCurrent():
     time.sleep(1)
     now = datetime.now()
     timeNow = datetime.strftime(now, "%H,%M,%S")
     print(timeNow)
     return timeNow
-def setDay(current, dia, lista):
-    dia = int(input('Que dia da semana é hoje? '))
-    if dia == 'Monday':
-        lista.append(aula1)
+def setDay():
+    if currentDay == 'Monday':
+        lista.append(aula1), lista.append(ingles), lista.append(aula2), lista.append(portugues), lista.append(aula4), lista.append(marketing)
         return lista
-    elif dia == 'Tuesday':
-        alarm1 = datetime(year=current.year, month=current.month, day=current.day, hour=8, minute=30, second=00)
-        link1 = ''
-        lista.append(datetime.strftime(alarm1, "%H,%M,%S"))
-        lista.append(link1)
-        alarm2 = datetime(year=current.year, month=current.month, day=current.day, hour=9, minute=30, second=00)
-        link2 = ''
-        lista.append(datetime.strftime(alarm2, "%H,%M,%S"))
-        lista.append(link2)
-        alarm3 = datetime(year=current.year, month=current.month, day=current.day, hour=10, minute=10, second=00)
-        link3 = ''
-        lista.append(datetime.strftime(alarm3, "%H,%M,%S"))
-        lista.append(link3)
-        alarm4 = datetime(year=current.year, month=current.month, day=current.day, hour=10, minute=40, second=00)
-        link4 = ''
-        lista.append(datetime.strftime(alarm4, "%H,%M,%S"))
-        lista.append(link4)
-        alarm5 = datetime(year=current.year, month=current.month, day=current.day, hour=13, minute=30, second=00)
-        link5 = ''
-        lista.append(datetime.strftime(alarm5, "%H,%M,%S"))
-        lista.append(link5)
-        alarm6 = datetime(year=current.year, month=current.month, day=current.day, hour=14, minute=00, second=00)
-        link6 = ''
-        lista.append(datetime.strftime(alarm6, "%H,%M,%S"))
-        lista.append(link6)
-        alarm7 = datetime(year=current.year, month=current.month, day=current.day, hour=15, minute=10, second=00)
-        link7 = ''
-        lista.append(datetime.strftime(alarm7, "%H,%M,%S"))
-        lista.append(link7)
+    elif currentDay == 'Tuesday':
+        lista.append(aula1), lista.append(matematica), lista.append(aula3), lista.append(sociologia), lista.append(aula4), lista.append(geografia), lista.append(aula5), lista.append(ingles), lista.append(aula6), lista.append(biologia), lista.append(aula7), lista.append(direito), lista.append(aula8), lista.append(gfo)
         return lista
-    elif dia == "Wednesday":
-        alarm1 = datetime(year=current.year, month=current.month, day=current.day, hour=8, minute=30, second=00)
-        link1 = ''
-        lista.append(datetime.strftime(alarm1, "%H,%M,%S"))
-        lista.append(link1)
-        alarm2 = datetime(year=current.year, month=current.month, day=current.day, hour=9, minute=00, second=00)
-        link2 = ''
-        lista.append(datetime.strftime(alarm2, "%H,%M,%S"))
-        lista.append(link2)
-        alarm3 = datetime(year=current.year, month=current.month, day=current.day, hour=10, minute=10, second=00)
-        link3 = ''
-        lista.append(datetime.strftime(alarm3, "%H,%M,%S"))
-        lista.append(link3)
+    elif currentDay == "Wednesday":
+        lista.append(aula1), lista.append(filosofia), lista.append(aula2), lista.append(contabilidade), lista.append(aula4), lista.append(quimica)
         return lista
-    elif dia == "Thursday":
-        alarm1 = datetime(year=current.year, month=current.month, day=current.day, hour=14, minute=50, second=00)
-        link1 = ''
-        lista.append(datetime.strftime(alarm1, "%H,%M,%S"))
-        lista.append(link1)
-        alarm2 = datetime(year=current.year, month=current.month, day=current.day, hour=9, minute=30, second=00)
-        link2 = ''
-        lista.append(datetime.strftime(alarm2, "%H,%M,%S"))
-        lista.append(link2)
-        alarm3 = datetime(year=current.year, month=current.month, day=current.day, hour=10, minute=10, second=00)
-        link3 = ''
-        lista.append(datetime.strftime(alarm3, "%H,%M,%S"))
-        lista.append(link3)
-        alarm4 = datetime(year=current.year, month=current.month, day=current.day, hour=10, minute=40, second=00)
-        link4 = ''
-        lista.append(datetime.strftime(alarm4, "%H,%M,%S"))
-        lista.append(link4)
-        alarm5 = datetime(year=current.year, month=current.month, day=current.day, hour=13, minute=30, second=00)
-        link5 = ''
-        lista.append(datetime.strftime(alarm5, "%H,%M,%S"))
-        lista.append(link5)
-        alarm6 = datetime(year=current.year, month=current.month, day=current.day, hour=14, minute=00, second=00)
-        link6 = ''
-        lista.append(datetime.strftime(alarm6, "%H,%M,%S"))
-        lista.append(link6)
-        alarm7 = datetime(year=current.year, month=current.month, day=current.day, hour=15, minute=10, second=00)
-        link7 = ''
-        lista.append(datetime.strftime(alarm7, "%H,%M,%S"))
-        lista.append(link7)
+    elif currentDay == "Thursday":
+        lista.append(aula1), lista.append(historia), lista.append(aula3), lista.append(filosofia), lista.append(aula4), lista.append(edfisica), lista.append(aula5), lista.append(sociologia), lista.append(aula6), lista.append(fisica), lista.append(aula7), lista.append(projeto), lista.append(aula8), lista.append(matematica)
         return lista
-    elif dia == 'Friday':
-        alarm1 = datetime(year=current.year, month=current.month, day=current.day, hour=8, minute=30, second=00)
-        link1 = ''
-        lista.append(datetime.strftime(alarm1, "%H,%M,%S"))
-        lista.append(link1)
-        alarm2 = datetime(year=current.year, month=current.month, day=current.day, hour=9, minute=00, second=00)
-        link2 = ''
-        lista.append(datetime.strftime(alarm2, "%H,%M,%S"))
-        lista.append(link2)
-        alarm3 = datetime(year=current.year, month=current.month, day=current.day, hour=10, minute=10, second=00)
-        link3 = ''
-        lista.append(datetime.strftime(alarm3, "%H,%M,%S"))
-        lista.append(link3)
-        alarm4 = datetime(year=current.year, month=current.month, day=current.day, hour=10, minute=40, second=00)
-        link4 = ''
-        lista.append(datetime.strftime(alarm4, "%H,%M,%S"))
-        lista.append(link4)
-        return lista[alarm1, link1, alarm2, link2, alarm3, link3, alarm4, link4]
-lista = setDay(current, dia, lista)
+    elif currentDay == 'Friday':
+        lista.append(aula1), lista.append(geografia), lista.append(aula2), lista.append(espanhol), lista.append(aula4), lista.append(portugues), lista.append(aula5), lista.append(fisica)
+        return lista
+lista = setDay()
 c = int(0)
 print(lista)
 print(lista[c])
 print(lista[c+1])
-while c <= int(len(lista)/2):
+while c <= int(len(lista)):
     timeNow = getCurrent()
-    if timeNow == lista[c] or timeNow == aula2 or timeNow == aula3 or timeNow == aula4 or timeNow == aula5 or timeNow == aula6 or timeNow == aula7:
+    if timeNow == lista[c]:
         webbrowser.open(lista[c+1])
-        driver.get(lista[c+1])
-        button = driver.find_element_by_xpath('/html/body/div[1]/c-wiz/div/div/div[9]/div[3]/div/div/div[3]/div/div/div[2]/div/div[2]/div/div/div[1]/div[2]')
-        button.click()
-        c = c + 1
+        c += 2
         print(c)
     else:
         c = c
         print(c)
-    
--- scripts/try.py
+++ scripts/try.py
@@ -1,8 +1,60 @@
+from datetime import datetime
+from datetime import date
+import time
+import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
+
+a = True
 lista = []
-a = 10
-b = 20
-def teste():
-    lista.append([a, b])
-    return lista
-lista = teste()
-print(lista[0])
+dia = str()
+current = date.today()
+currentDay = datetime.strftime(current, "%A")
+
+aula1 = datetime.strftime(datetime(year=current.year, month=current.month, day=current.day, hour=8, minute=30, second=00), "%H,%M,%S")
+aula2 = datetime.strftime(datetime(year=current.year, month=current.month, day=current.day, hour=9, minute=00, second=00), "%H,%M,%S")
+aula3 = datetime.strftime(datetime(year=current.year, month=current.month, day=current.day, hour=9, minute=30, second=00), "%H,%M,%S")
+aula4 = datetime.strftime(datetime(year=current.year, month=current.month, day=current.day, hour=10, minute=10, second=00), "%H,%M,%S")
+aula5 = datetime.strftime(datetime(year=current.year, month=current.month, day=current.day, hour=10, minute=40, second=00), "%H,%M,%S")
+aula6 = datetime.strftime(datetime(year=current.year, month=current.month, day=current.day, hour=13, minute=30, second=00), "%H,%M,%S")
+aula7 = datetime.strftime(datetime(year=current.year, month=current.month, day=current.day, hour=14, minute=00, second=00), "%H,%M,%S")
+aula8 = datetime.strftime(datetime(year=current.year, month=current.month, day=current.day, hour=15, minute=10, second=00), "%H,%M,%S")
+
+historia = ''
+geografia = ''
+portugues = ''
+ingles = ''
+matematica = ''
+marketing = ''
+contabilidade = ''
+gfo = ''
+fisica = ''
+sociologia = ''
+biologia = ''
+direito = ''
+filosofia = ''
+quimica = ''
+historia = ''
+edfisica = ''
+espanhol = ''
+
+
+diviser = int(2)
+quantity = int()
+def getCurrent():
+    time.sleep(1)
+    now = datetime.now()
+    timeNow = datetime.strftime(now, "%H,%M,%S")
+    print(timeNow)
+    return timeNow
+def setDay():
+    dia = int(input('Que dia da semana é hoje? '))
+    if dia == 'Monday':
+        lista.append(aula1), lista.append(ingles), lista.append(aula2), lista.append(portugues), lista.append(aula4), lista.append(marketing)
+        return lista
+    else:
+        lista.append(aula1), lista.append(ingles), lista.append(aula2), lista.append(portugues), lista.append(aula4), lista.append(marketing)
+        return lista 
+
+print(currentDay == 'Tuesday')
